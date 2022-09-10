d97d93bc878f0b6d4520cbfa53e5565508f947cf
Artak1999 <dashyan.1999@mail.ru>
2022-09-10 13:44:46 +0500

    Hard task.

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -7,8 +7,9 @@ public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
         Map<String, Integer> map = new HashMap<>();
         for(Map.Entry<Integer, String> i : sourceMap.entrySet()){
-            map.put(i.getValue(),i.getKey());
+            map.put(i.getValue(), i.getKey());
         }
+        map.remove("Friday",10);
         return map;
     }
 }
